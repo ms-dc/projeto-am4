a1b64afa0f53b52f6c76ca5c927f4f0e4da7d509
xReiketsu <matheus.santana.da.costa@gmail.com>
2020-05-22 23:36:56 -0300

    database

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     // 'default' => env('DB_CONNECTION', 'mysql'),
 
@@ -68,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'ec2-18-233-32-61.compute-1.amazonaws.com'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'd7ilqgrccokqcm'),
-            'username' => env('DB_USERNAME', 'rbqqhunranaswb'),
-            'password' => env('DB_PASSWORD', '09df74fcf0dd7bee30795c604ebc28ba9d3aa3815daba02cbd4b23c72254abab'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
